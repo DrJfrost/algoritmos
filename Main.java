188d0b9eb9a74faaa3ec3820392a74b14a6d8194
julian rojas <julianrojas99@hotmail.com>
2018-08-10 07:58:16 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,65 @@
+package ordenadorMatrices;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static ArrayList<Integer> vector;
+	
+	public static void main(String[] args) {
+		
+		vector = new ArrayList<Integer>();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a limit ");			
+		String limite = scan.nextLine();
+		fill(Integer.parseInt(limite));
+		
+		long startTime = System.nanoTime();
+		vector=selectionAlgorythm2(vector);
+		long endTime = System.nanoTime();
+
+		long duration = (endTime - startTime);
+		
+		for (int i=0; i<vector.size(); i++) {
+			System.out.println(vector.get(i) + "");
+		}
+		System.out.println(duration/1000000 + " milliseconds");
+
+	}
+	
+	public static void fill (int limite) { 
+		for (int i = 0; i<limite; i++) {
+			vector.add((int)(Math.random() * 10001));
+		}
+	}
+	
+	public static ArrayList<Integer> selectionAlgorythm(ArrayList<Integer> array){
+			for (int i = 0; i < array.size(); i++) {
+				for (int j = i + 1; j< array.size(); j++) {
+					if(array.get(i)>array.get(j)) {
+						int temp = array.get(i);
+						array.set(i, array.get(j));
+						array.set(j, temp);
+					}
+				}
+			}
+		return array;
+		
+	}
+	
+	public static ArrayList<Integer> selectionAlgorythm2(ArrayList<Integer> array){
+		for (int i = 0; i < array.size(); i++) {
+			for (int j = i + 1; j< array.size(); j++) {
+				if(array.get(i)<array.get(j)) {
+					int temp = array.get(i);
+					array.set(i, array.get(j));
+					array.set(j, temp);
+				}
+			}
+		}
+	return array;
+	
+}
+
+}
